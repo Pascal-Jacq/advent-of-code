6640d5582c1210947154b090fd5e82bf8ac246a8
Pascal JACQ <pascal.jacq29@gmail.com>
2022-12-05 09:47:21 +0100

    codespaces compliant devcontainer

--- .devcontainer/generate_dockerfile.sh
+++ .devcontainer/generate_dockerfile.sh
@@ -9,7 +9,7 @@ then
 FROM python:3.11-slim
 
 # Just need ps to handle container lifetime
-RUN apt-get update && apt-get install -y procps --no-install-recommends && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y procps git --no-install-recommends && rm -rf /var/lib/apt/lists/*
 
 COPY requirements.txt .
 
